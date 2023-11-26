b46fc8e247b8758d1b9d8f5b94295f73a978173f
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2023-11-26 12:06:45 +0800

    Add logging to moneyScript, remove TS comment

--- moneyScript.ts
+++ moneyScript.ts
@@ -1,8 +1,8 @@
 import { NS } from "@ns";
 
-/** @param {NS} ns */
 export async function main(ns: NS) {
     const host = String(ns.args[0]);
+    ns.print(`Running moneyScript on: ${host}`);
     // eslint-disable-next-line no-constant-condition
     while (true) {
         await ns.hack(host);
