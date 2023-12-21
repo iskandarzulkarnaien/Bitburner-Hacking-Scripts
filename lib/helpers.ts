bb8b98baef85d1a363b5eaa4abf69e9a529731dc
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2023-12-22 00:26:22 +0800

    Fix gitignore and commit missing files

---
+++ lib/helpers.ts
@@ -0,0 +1,42 @@
+import { NS } from "@ns";
+
+export function getNumExecutableThreads(ns: NS, host: string, malware: string, reservedRam=0) {
+    return Math.trunc((ns.getServerMaxRam(host) - ns.getServerUsedRam(host) - reservedRam) / ns.getScriptRam(malware));
+}
+
+export function execMaxOn(ns: NS, host: string, malware: string, target?: string, reservedRam=0) {
+    if (!target) target = host;
+
+    ns.scp(malware, host);
+    ns.exec(malware, host, getNumExecutableThreads(ns, host, malware, reservedRam), target)
+}
+
+export function getThresholdMoneyAmount(ns: NS, threshold: number) {
+    return ns.getServerMoneyAvailable('home') / threshold
+}
+
+export function getMaxMoneyHost(ns: NS) {
+    const host = String(ns.readPort(1))
+    if (host == 'NULL PORT DATA') return 'n00dles';
+
+    ns.writePort(1, host)
+    return host
+}
+
+export function setMaxMoneyHost(ns: NS, host: string) {
+    ns.writePort(1, host)
+}
+
+export function nth(n: number): string {
+    return ["st","nd","rd"][((n + 90) % 100 - 10) % 10 - 1] || "th"
+}
+
+export function applyPadding(str: string) {
+    return `${getPadding()}${str}`
+}
+
+const timestampPadding = 16 + 1 // Format: "[MM-dd HH:mm:ss]" 16 char + 1 whitespace
+const defaultPadding = 4
+export function getPadding(numSpaces = timestampPadding + defaultPadding) {
+    return " ".repeat(numSpaces) 
+}
---
+++ lib/ns_container.ts
@@ -0,0 +1,8 @@
+import { NS } from "@ns";
+
+export class NSContainer {
+    ns: NS
+    constructor(ns: NS) {
+        this.ns = ns
+    }
+}
