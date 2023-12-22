b8dc27e69e405614fdbe02a704a8060a7df671d7
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2023-12-22 10:34:24 +0800

    Add method not implemented error

--- lib/error_messages.ts
+++ lib/error_messages.ts
@@ -1,2 +1,3 @@
 // Miscellaneous
 export const unreachableConditionalPathReached = () => 'Conditional evaluated to invalid path'
+export const methodNotImplemented = (className: string, methodName: string) => `Method ${methodName} of class ${className} is not implemented`
