e00ee1c51c54ab9cd8a1e978bb942111b099c8d7
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2023-12-22 10:34:06 +0800

    Rename construct name to get name, change to non-protected

--- gang/names/name.ts
+++ gang/names/name.ts
@@ -5,10 +5,10 @@ export abstract class Name {
         throw new Error(methodNotImplemented(Name.constructor.name, Name.generate.name))
     }
 
+    abstract getName(): string
+
     /**
      * Method to update the name based on various conditions (e.g. rank) or have names that "glitch out"
      */
     abstract updateName(...params: Array<unknown>): string
-
-    protected abstract constructName(): string
 }
